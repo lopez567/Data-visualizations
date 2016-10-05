81a1d2ad51a75c97fd432f63106e9ac205a3f1c7
lopez567 <spencer.r.lopez@gmail.com>
2016-10-04 22:32:44 -0700

    Added clustering algorithm

--- animals.js
+++ animals.js
@@ -34,22 +34,30 @@ var updateAnimals = function(data) {
 		.attr('cx', d => d.brainWeight * 2)
 		.attr('cy', d => 500 - d.weight * 2)
 		.attr('r', 3)
-		.attr('class', d => d.animal + ' animals')
+		.attr('class', d => 'animals')
 }
 
 updateAnimals(animalData);
 
-var randomPoint = function() {
-	this.weight = Math.floor(Math.random() * 240);
-	this.brainWeight = Math.floor(Math.random() * 240);
+var randomCoord = function() {
+	return Math.floor(Math.random() * 240);
 }
 
+var randomPoint = function(color) {
+	this.weight = randomCoord();
+	this.brainWeight = randomCoord();
+	this.class = color;
+}
+
+// if you wanna sound smart, call this the...
+//EUCLIDEAN DISTANCE FUNCTION
 var getDistance = function(point1, point2) {
 	return Math.sqrt(Math.pow(point2.weight - point1.weight, 2) + Math.pow(point2.brainWeight - point1.brainWeight, 2));
 }
 
 var newAnimal = new randomPoint();
 
+//K-neighbors algorithm: Trained data set
 var guessSpecies = function(animal , k) {
 	var guessingAnimal = g.selectAll('circle')
 		.filter('.guess')
@@ -84,6 +92,7 @@ var guessSpecies = function(animal , k) {
 	g.selectAll('circle')
 		.filter('.animals')
 		.data(animalData, d => d.id)
+		.attr('class', d => d.animal + ' animals')
 		.classed('selected', d => {
 			for (var i = 0; i < kNear.length; i ++) {
 				if (d.weight === kNear[i].data.weight && d.brainWeight === kNear[i].data.brainWeight ) {
@@ -113,4 +122,92 @@ var guessSpecies = function(animal , k) {
 
 	console.log(animalGuess.animal);
 
+}
+
+
+var clusterSpieces = function(k) {
+	var clusterPoints = [];
+	//Create k random cluster points
+	for (var i = 0; i < k; i++) {
+		var newCluster = new randomPoint(i);
+		newCluster.xKids = [];
+		newCluster.yKids = [];
+		clusterPoints.push(newCluster);
+	}
+
+	var clusters = g.selectAll('circle')
+		.filter('.cluster')
+		.data(clusterPoints, d => d.class)
+
+
+	clusters.enter().append('circle')
+		.attr('cx', d => d.brainWeight * 2)
+		.attr('cy', d => 500 - d.weight * 2)
+		.attr('r', 7)
+		.attr('class', d => 'a' + d.class + ' cluster')
+
+	var animals = g.selectAll('circle')
+		.filter('.animals')
+		.data(animalData, d => d.id)
+
+	//Assign the animals their nearest cluster >> Move to clusters to their average >> repeat until no more movement 
+
+	var clusterThoseBadBoys = function() {
+		var repeat = false;
+		animals.attr('class', d => {
+			var nearestCluster = {distance: 500, cluster: 'obj'};
+			clusterPoints.forEach( point => {
+				var distance = getDistance(d, point)
+				if (distance <= nearestCluster.distance) {
+					nearestCluster.distance = distance;
+					nearestCluster.cluster = point;
+				}
+			})
+			nearestCluster.cluster.xKids.push(d.brainWeight);
+			nearestCluster.cluster.yKids.push(d.weight);
+			return 'a' + nearestCluster.cluster.class;
+		})
+
+		//Define new cluster centers
+		clusterPoints.forEach( point => {
+			if (point.xKids.length === 0) {
+				point.newX = randomCoord();
+				point.newY = randomCoord();
+			} else {
+				var xSum = point.xKids.reduce( (a, b) => a + b);
+				var ySum = point.yKids.reduce( (a, b) => a + b);
+				point.newX = xSum / point.xKids.length;
+				point.newY = ySum / point.yKids.length;
+				point.xKids = [];
+				point.yKids = [];
+			}
+			if (point.newX !== point.brainWeight || point.newY !== point.weight) {
+				repeat = true;
+			}
+			point.brainWeight = point.newX;
+			point.weight = point.newY;
+		})
+		
+
+		g.selectAll('circle').filter('.cluster')
+			.transition()
+			.duration(500)
+			.attr('cx', d => d.brainWeight * 2)
+			.attr('cy', d => 500 - d.weight * 2)
+
+		if (repeat) {
+			setTimeout(()=>{clusterThoseBadBoys();},500);
+		}	
+	}
+
+	clusterThoseBadBoys(); //!!!!!!!!!
+}
+
+
+var reset = function() {
+	var data = g.selectAll('circle')
+		.data(animalData, d => d.id)
+		.attr('class', 'animals');
+
+	data.exit().remove();
 }
